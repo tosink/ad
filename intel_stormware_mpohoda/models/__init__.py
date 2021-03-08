010e9f96d29aed37cfa58ff6de48f1dfbd8ff6c1
dontk1 <komolafetosin@gmail.com>
2021-03-08 09:22:48 +0100

    mpohoda

--- intel_stormware_mpohoda/models/__init__.py
+++ intel_stormware_mpohoda/models/__init__.py
@@ -1,6 +1,6 @@
 from . import base_config_settings
 from . import mpohoda
 from . import company
-from . import acount_invoice
+from . import account_invoice
 # from . import res_partner
 # from . import res_users
