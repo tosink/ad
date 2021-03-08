2115ade672e3b6957cf6a5e792cf59a3e5b90ee3
dontk1 <komolafetosin@gmail.com>
2021-03-08 09:40:49 +0100

    mpohoda

--- intel_stormware_mpohoda/models/account_tax.py
+++ intel_stormware_mpohoda/models/account_tax.py
@@ -5,7 +5,7 @@ from odoo import api, fields, models, _, SUPERUSER_ID
 class AccountTax(models.Model):
     _inherit = 'account.tax'
 
-    mpohoda_vat = fields.Selection(historyLow
+    mpohoda_vat = fields.Selection(
         [('none','None'),('high','high'),('low','Low'),('third','Third'),
         ('historyHigh','History High'),('historyLow','History Low'),('historyThird','History Third')],
         string='Mpohoda Rate Vat', 
