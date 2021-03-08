96112076e3907385015b13629ff028a31c8e7e11
dontk1 <komolafetosin@gmail.com>
2021-03-08 10:07:55 +0100

    mpohoda

--- intel_stormware_mpohoda/models/mpohoda.py
+++ intel_stormware_mpohoda/models/mpohoda.py
@@ -17,7 +17,7 @@ class MpohodaPaymentType(models.Model):
     company_id = fields.Many2one(
         comodel_name='res.company', 
         string='Company', 
-        required=True,
+        required=False,
         default=lambda self: self.env['res.company']._company_default_get('mpohoda.payment.type'))
 
 
@@ -41,7 +41,7 @@ class MpohodaInvoiceType(models.Model):
     company_id = fields.Many2one(
         comodel_name='res.company', 
         string='Company', 
-        required=True,
+        required=False,
         default=lambda self: self.env['res.company']._company_default_get('mpohoda.invoice.type'))
 
 
