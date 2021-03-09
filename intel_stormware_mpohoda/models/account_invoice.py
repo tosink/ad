27bf78acf6a6e5c0f5b2b7291037434caed8eb37
dontk1 <komolafetosin@gmail.com>
2021-03-09 21:58:05 +0100

    mpohoda

--- intel_stormware_mpohoda/models/account_invoice.py
+++ intel_stormware_mpohoda/models/account_invoice.py
@@ -199,7 +199,7 @@ class AccountInvoice(models.Model):
                     'res_id': self.id,
                     'mimetype':'application/pdf',
                     'type':'binary',
-                    'datas':base64.b64encode(response.text.encode('utf8')),
+                    'datas':base64.b64encode(response.text.encode('utf8')).decode('utf8'),
                     'description':'MPOHODA'
                 })
                 self.document_generated = True
