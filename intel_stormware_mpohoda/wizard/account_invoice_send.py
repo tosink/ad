56863fda034dac9e23f18e0eee43112e83b7d4ba
dontk1 <komolafetosin@gmail.com>
2021-03-10 17:11:37 +0100

    mpohoda

--- intel_stormware_mpohoda/wizard/account_invoice_send.py
+++ intel_stormware_mpohoda/wizard/account_invoice_send.py
@@ -11,20 +11,25 @@ _logger = logging.getLogger(__name__)
 class AccountInvoiceSend(models.TransientModel):
     _inherit = "account.invoice.send"
 
+    get_document = fields.Boolean(
+        string='Get POHODA Document?')
+
+    @api.onchange('get_document')
     def get_mpohoda_document(self):
-        active_id = self.env.context.get('active_id', False)
-        if self.env.context.get('params', False):
-            params = self.env.context.get('params', False)
-            if params['model'] == 'account.invoice':
-                active_id = params['id']
-        invoice = self.env["account.invoice"].browse(active_id)
-        
-        mpohoda_attachment_ids = self.env["ir.attachment"].search(
-            [
-                ("res_model", "=", 'account.invoice'),
-                ("res_id", "=", invoice.id),
-                ("description","=","MPOHODA")
-            ]
-        )
-        if mpohoda_attachment_ids:
-            self.attachment_ids = [(6,0,mpohoda_attachment_ids.ids)]
+        if self.get_document:
+            active_id = self.env.context.get('active_id', False)
+            if self.env.context.get('params', False):
+                params = self.env.context.get('params', False)
+                if params['model'] == 'account.invoice':
+                    active_id = params['id']
+            invoice = self.env["account.invoice"].browse(active_id)
+            
+            mpohoda_attachment_ids = self.env["ir.attachment"].search(
+                [
+                    ("res_model", "=", 'account.invoice'),
+                    ("res_id", "=", invoice.id),
+                    ("description","=","MPOHODA")
+                ]
+            )
+            if mpohoda_attachment_ids:
+                self.attachment_ids = [(6,0,mpohoda_attachment_ids.ids)]
